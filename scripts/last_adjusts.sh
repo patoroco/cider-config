ea3147ba188837f8b72b819b77ad588f682caad3
Jorge Maroto <patoroco@gmail.com>
2015-10-18 19:49:26 +0200

    Update cocoapods repo and install Xcode tools

--- scripts/last_adjusts.sh
+++ scripts/last_adjusts.sh
@@ -4,6 +4,12 @@ set -eu
 set -o pipefail
 
 
+echo "Installing Xcode Command Line Tools"
+xcode-select --install
+
+echo "Seting up cocoapods"
+pod setup
+
 echo "Installing vim plugins"
 git submodule init
 git submodule update
